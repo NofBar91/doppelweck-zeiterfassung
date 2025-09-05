f6586e668f8468dedce71cde3dd237c515a84432
Sehmer Thorsten <thorstenstoffel@web.de>
2025-09-05 14:33:28 +0200

    middleware.ts doppelt gelöscht

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: [
-    "/dashboard",
-    "/admin/:path*",
-    // später: "/api/time-entries/:path*" wenn du API schützen willst
-  ],
-};
